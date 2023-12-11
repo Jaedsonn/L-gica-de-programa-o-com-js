5fc9c94425404430074415ae1b32e3c5117f8e29
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2023-12-11 19:04:54 -0300

    add

---
+++ livrojs/cap05/nodejs/ex5_9.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")()
+console.log("Programa etiqueta")
+console.log("---------------------------------------------------")
+const nomeProduto = prompt("Nome do produto: ")
+const etiquetas = Number(prompt("Número de etiquetas"))
+
+for(let i = 1; i <= etiquetas / 2; i++){
+    console.log(`${nomeProduto} ${nomeProduto}`)
+}
+
+if(etiquetas % 2 == 1){
+    console.log(nomeProduto)
+}
