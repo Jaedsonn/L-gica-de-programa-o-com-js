4c5f16030632950854a9803b898e7875e11827a8
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2023-12-11 20:33:21 -0300

    change

---
+++ livrojs/cap05/nodejs/ex5_a.js
@@ -0,0 +1,8 @@
+ const prompt = require("prompt-sync")() 
+const fruta = prompt("Fruta: ")
+const num = Number(prompt("Número: "))
+
+for(let i = 1; i <= num; i++){
+   console.log( `${fruta}*`)
+}
+
---
+++ livrojs/cap05/nodejs/ex5_b.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")()
+let nChinchila = Number(prompt("N° chinchila"))
+const anos = Number(prompt("N° anos"))
+let crescimento = nChinchila * 3  
+
+for(let i = 1; i <= anos; i++){
+    for(let i = 1; i <= anos; ){
+    
+
+    }
+}
