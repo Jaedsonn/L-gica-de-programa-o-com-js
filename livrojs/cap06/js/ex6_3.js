0575a54ac35436a8323601326d01754d59ae67c9
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2023-12-24 22:40:47 -0300

    Finalizando o Ex 6.3 do cap 6.8

--- livrojs/cap06/js/ex6_3.js
+++ livrojs/cap06/js/ex6_3.js
@@ -1,5 +1,5 @@
 const frm = document.querySelector("form")
-const reps = document.querySelector("pre")
+const resp = document.querySelector("pre")
 const carros = []
 
 frm.addEventListener("submit", (e) =>{
@@ -7,7 +7,56 @@ frm.addEventListener("submit", (e) =>{
     const modelo = frm.inModelo.value
     const preco = Number(frm.inPreco.value)
     carros.push({modelo, preco})
-    frm.inModelo = " "
-    frm.inPreco = " "
-    inModelo.focus()
+    frm.inModelo.value = ""
+    frm.inPreco.value = ""
+    frm.inModelo.focus()
+    //dispara um evento de click em btListar (equivale a um click no botão na página)
+    frm.btListar.dispatchEvent(new Event("click"))  
 })
+
+frm.btListar.addEventListener("click", () => {
+    if(carros.length == 0){
+        alert("Não há carros na lista")
+        return
+    }
+
+    const lista = carros.reduce((acumulador, carro) => 
+    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+
+    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
+})
+
+frm.btFiltrar.addEventListener("click", (e) =>{
+    const maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"))
+    if(maximo == 0 || isNaN(maximo)){
+        return
+    }
+
+    //cria um vetor que atende as condições do filtro
+    const carrosFilter = carros.filter(carro => carro.preco <= maximo)
+    if(carrosFilter.length == 0){
+        alert("Não há carros com o preço inferior ou igual ao solicitado")
+        return
+    }
+    let lista = ""
+    for(const carro of carrosFilter){
+        lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+    }
+    resp.innerText = `Carros Até R$: ${maximo.toFixed(2)}\n${"-".repeat(40)}\n${lista}`
+})
+
+frm.btSimular.addEventListener("click", () => {
+    const desconto = Number(prompt("Qual o percentual de desconto: "))
+    if (desconto == 0 || isNaN(desconto)) {    // se não informou ou valor inválido
+      return                                   // ... retorna
+    }
+    const carrosDesc = carros.map(aux => ({
+      modelo: aux.modelo,
+      preco: aux.preco - (aux.preco * desconto / 100)
+    }))
+    let lista = ""
+    for (const carro of carrosDesc) {       // percorre cada elemento do array 
+      lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+    }
+    resp.innerText = `Carros com desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
+  })
