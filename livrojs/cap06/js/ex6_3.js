7fdf924055afb62ceae35432a21379101f548a51
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2023-12-25 10:53:00 -0300

    finalizando o exercício 6.3

--- livrojs/cap06/js/ex6_3.js
+++ livrojs/cap06/js/ex6_3.js
@@ -20,9 +20,9 @@ frm.btListar.addEventListener("click", () => {
         return
     }
 
-    const lista = carros.reduce((acumulador, carro) => 
-    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
-
+     const lista = carros.reduce((acumulador, carro) => 
+        acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+ 
     resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
 })
 
