c5597bf126f64a143f723379f110b10e40dc54da
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-01-01 14:01:01 -0300

    ex 6.7 finalizado com sucesso

--- livrojs/cap06/nodejs/ex6_7.js
+++ livrojs/cap06/nodejs/ex6_7.js
@@ -1,26 +1,29 @@
-const prompt = require("prompt-sync")()
-const saques = []
-let valor
-let situacao = ""
-do{
-    valor = Number(prompt("Valor: "))
-    if(valor % 10 == 0 ){
-        situacao = "Válido"
-        console.log(situacao + "\n" + "-".repeat(40))
-    } else{
-        situacao = "Inválido"
-        console.log(situacao + "\n" + "-".repeat(40))
-    }
-    saques.push({valor, situacao})
-} while(valor !== 0)
-saques.pop()
-
-const validos = saques.filter(valeu => valeu.situacao == "Válido")
-const invalidos = saques.filter ( nvaleu => nvaleu.situacao == "Inválido")
-
-const sumValidos = validos.reduce((aux,valor) => aux + valor.valor)
-const sumInvalidos = invalidos.reduce((aux, valor)=> aux + valor.valor)
-
-console.log(sumValidos)
-/* console.log(validos)
-console.log(invalidos) */
+const prompt = require("prompt-sync")();
+console.log("Informe o valor dos saques ou 0 para sair")
+const saques = [];
+let valor;
+let situacao = "";
+do {
+  valor = Number(prompt("Valor: "));
+  if (valor % 10 == 0) {
+    situacao = "Válido";
+    console.log("Saque realizado com Sucesso"+ "\n");
+  } else {
+    situacao = "Inválido";
+    console.log("Erro...Saque "+ situacao + "\n");
+  }
+  saques.push({ valor, situacao });
+} while (valor !== 0);
+saques.pop();
+const validos = saques.filter((valeu) => valeu.situacao == "Válido");
+const invalidos = saques.filter((nvaleu) => nvaleu.situacao == "Inválido");
+const sumValidos = validos.reduce((aux, ponto) => aux + ponto.valor, 0);
+console.log("Os saques válidos foram:\n" + "-".repeat(40));
+const listaValidos = validos.map((valido) => valido.valor);
+for (const lista of listaValidos) {
+  console.log("Valor: R$" + lista);
+}
+console.log("-".repeat(40))
+console.log(
+  `O total de saques válidos foi: R$ ${sumValidos.toFixed(2)} \nN° de tentativas de Saque: ${invalidos.length}`
+);
