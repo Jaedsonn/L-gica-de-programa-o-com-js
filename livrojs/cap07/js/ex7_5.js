5258696adf9576519f35002389ea70a7cedf44ba
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-01-09 21:32:25 -0300

    Ex 7.5 finalizado junto com leitura do método matc

---
+++ livrojs/cap07/js/ex7_5.js
@@ -0,0 +1,21 @@
+const frm = document.querySelector("form")
+const outPut = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const nome =  frm.inNome.value
+
+    const parte = nome.split(" ")
+    let email = ""
+    const tam = parte.length
+
+    for(let i = 0; i < tam - 1; i ++){
+        email += parte[i].charAt(0)
+    }
+
+    email += parte[tam - 1] + "@empresa.com.br"
+
+    outPut.innerHTML = ` Email: ${email.toLowerCase()}`
+
+})
+
