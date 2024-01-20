21d3b8de2835590927d21bff9fae66e06f556bff
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-01-20 19:45:51 -0300

    Exercício 7.a finalizado

--- livrojs/cap07/js/ex7_a.js
+++ livrojs/cap07/js/ex7_a.js
@@ -5,10 +5,11 @@ let par = ""
 let impar = ""
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
+    resp.innerHTML = " "
     const mensagem = frm.inMensagem.value
     const partes = mensagem.split("")
     const tam = mensagem.length - 1
-
+    
     for (let i = 0; i <= tam; i++) {
         if(i % 2 == 0 || i == 0){
             par += partes[i]
@@ -16,5 +17,16 @@ frm.addEventListener("submit", (e) => {
             impar += partes[i]
         }
     }
-    resp.innerHTML = par + " " + impar
+    const par2 = par.split(" ").join("")
+    const impar2 = impar.split(" ").join("")
+    
+    const frase = par2 + " " + impar2
+    resp.innerHTML = frase
+    par = ""
+    impar = ""
+})
+
+frm.inBtn.addEventListener("click", (e)=>{
+    const mensagem = frm.inMensagem.value
+    resp.innerHTML = mensagem
 })
