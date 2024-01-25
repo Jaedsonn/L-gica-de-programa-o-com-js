a5cd73bd5d1b7c2e61319dd5eb8d188a26b7d070
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-01-24 21:32:28 -0300

    Exercício 7.c finalizado

--- livrojs/cap07/js/ex7_c.js
+++ livrojs/cap07/js/ex7_c.js
@@ -1,21 +1,31 @@
-const frm = document.querySelector("form")
-const dataLimite = document.querySelector("#inLimite")
-const desconto = document.querySelector("#inDesconto")
+const frm = document.querySelector("form") // obtém elementos da página
+const resp1 = document.querySelector("#outDataLimite")
+const resp2 = document.querySelector("#outComDesconto")
 
-frm.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const data = frm.inData.value
-    const multa = Number(frm.inMulta.value)
+frm.addEventListener("submit", (e) => {          // "escuta" evento submit do form
+  e.preventDefault()                             // evita envio do form
 
-    const partes = data.split("-")
-    const dia = partes[2]
-    const mes = partes[1]
-    const ano = partes[0]
+  const data = frm.inData.value
+  const valor = Number(frm.inValor.value)
 
-    const date2 = new Date()
-    date2.setDate(dia)
-    date2.setMonth(mes)
-    date2.setFullYear(ano)
-    
-    desconto.innerHTML = dia + "  " + mes + "  " + "  " + ano
+  // declara variável do tipo date
+  const dataLimite = new Date()
+
+  // obtém as partes da data da multa
+  const partes = data.split("-")
+  // "seta" as partes da data
+  dataLimite.setDate(Number(partes[2]))
+  dataLimite.setMonth(Number(partes[1]) - 1)
+  dataLimite.setFullYear(Number(partes[0]))
+
+  const dia = dataLimite.getDate()    // obtém o dia da multa
+
+  dataLimite.setDate(dia + 90)      // aumenta 90 dias na data da multa
+  const mes = dataLimite.getMonth() + 1
+  const ano = dataLimite.getFullYear()
+
+  const comDesconto = valor * 0.80
+
+  resp1.innerText = "Data Limite para Pagto com Desconto: " + (dia < 10 ? "0" + dia : dia) + "/" + (mes < 10 ? "0" + mes : mes) + "/" + ano
+  resp2.innerText = "Valor com Desconto R$: " + comDesconto.toFixed(2)
 })
