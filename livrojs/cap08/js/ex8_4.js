2c8837734a3fef7094b62a40bc4fed6c1b8ec32a
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-02-05 21:35:16 -0300

    trabalhando no exercício 8.4

--- livrojs/cap08/js/ex8_4.js
+++ livrojs/cap08/js/ex8_4.js
@@ -1,21 +1,30 @@
-const frm = document.querySelector("form")
-const resp = document.querySelector("pre")
+const frm = document.querySelector("form");
+const resp = document.querySelector("pre");
 
-const itens = []
+const itens = [];
 
+frm.rbPizza.addEventListener("click", () => {
+    const pizza = frm.inPizza;
+    const bebida = frm.inBebida;
+    pizza.className = "exibe";
+    bebida.className = "oculta";
+});
 
+frm.rbBebida.addEventListener("click", () => {
+    const pizza = frm.inPizza;
+    const bebida = frm.inBebida;
+    pizza.className = "oculta";
+    bebida.className = "exibe";
+});
 
-frm.rbPizza.addEventListener("click", ()=>{
-    const pizza = frm.inPizza
-    const bebida = frm.inBebida
-    pizza.className = "exibe"
-    bebida.className = "oculta"
+frm.inDetalhes.addEventListener("focus", () => {
+    if(frm.rbPizza.checked){
+        const pizza = frm.inPizza.value
+        const num = pizza == "media" ? 2 : pizza == "grande" ? 3  : 4
+        frm.inDetalhes.placeholder = `Até ${num} sabores`
+    }
 })
 
-frm.rbBebida.addEventListener("click", ()=>{
-    const pizza = frm.inPizza
-    const bebida = frm.inBebida
-    pizza.className = "oculta"
-    bebida.className = "exibe"
+frm.inDetalhes.addEventListener("blur", () =>{
+    frm.inDetalhes.placeholder = " "
 })
-
