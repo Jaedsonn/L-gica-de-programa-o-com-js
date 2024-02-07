eda3db479ac28eba3dbce42462a8604446ad7292
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-02-07 20:57:59 -0300

    ex 8.4 finalizado

--- livrojs/cap08/js/ex8_4.js
+++ livrojs/cap08/js/ex8_4.js
@@ -18,13 +18,31 @@ frm.rbBebida.addEventListener("click", () => {
 });
 
 frm.inDetalhes.addEventListener("focus", () => {
-    if(frm.rbPizza.checked){
+    if (frm.rbPizza.checked) {
         const pizza = frm.inPizza.value
-        const num = pizza == "media" ? 2 : pizza == "grande" ? 3  : 4
+        const num = pizza == "media" ? 2 : pizza == "grande" ? 3 : 4
         frm.inDetalhes.placeholder = `Até ${num} sabores`
     }
 })
 
-frm.inDetalhes.addEventListener("blur", () =>{
+frm.inDetalhes.addEventListener("blur", () => {
     frm.inDetalhes.placeholder = " "
+})
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let produto
+    if (frm.rbPizza.checked) {
+        const num = frm.inPizza.selectedIndex
+        produto = frm.inPizza.options[num].text
+    } else {
+        const num = frm.inBebida.selectedIndex
+        produto = frm.inBebida.options[num].text
+    }
+    const detalhes = frm.inDetalhes.value
+    itens.push(produto + "(" + detalhes + ")")
+    resp.innerText = itens.join("\n")
+
+    frm.reset()
+    frm.rbPizza.dispatchEvent(new Event("click"))
 })
