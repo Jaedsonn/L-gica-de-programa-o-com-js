b63c845f92507190d478edbc7cec73a9127a0fc4
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-02-21 21:42:18 -0300

    Exercício 9.c finalizado com dificuldade.Exp abaixo Não consegui compreende de maneira correta o que estava sendo pedido no Exercício, tentei fazer algo que não estava sendo requisitado e acabei perdendo tempo resolvendo um problem inexistente.

--- livrojs/cap09/js/resp9_c.js
+++ livrojs/cap09/js/resp9_c.js
@@ -1,47 +1,42 @@
 const frm = document.querySelector("form")
-const numServicos = document.querySelector("span")
-const tipoServico = document.querySelector("h4")
-
-
+const resp1 = document.querySelector("span")
+const resp2 = document.querySelector("h4")
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const servico = frm.inServico.value
-    if (!localStorage.getItem("serviço")) {
-        localStorage.setItem("serviço", servico)
-    } else {
-        const item = localStorage.getItem("serviço") + ";" + servico
-        localStorage.setItem("serviço", item)
-    }
+  e.preventDefault()                            
 
-    /*  numServicos.innerText = localStorage.getItem("serviço").split(";").length */
-    adicionarServicos()
-    frm.reset()
-    frm.inServico.focus()
+  const servico = frm.inServico.value
+  
+  if (localStorage.getItem("herbieServico")) {
+    localStorage.setItem("herbieServico", localStorage.getItem("herbieServico") + ";" + servico)
+  } else {
+    localStorage.setItem("herbieServico", servico)    
+  }
+  mostrarPendentes()
+  frm.reset()
+  frm.inServico.focus()
 })
 
-const adicionarServicos = () => {
-    const servicos = localStorage.getItem("serviço").split(";")
-    console.log(servicos)
-    numServicos.innerText = servicos == [''] ? 0 : servicos.length
+const mostrarPendentes = () => {
+  let numPendentes
+  if (localStorage.getItem("herbieServico")) {
+     numPendentes = localStorage.getItem("herbieServico").split(";").length
+  } else {
+    numPendentes = 0
+  }
+  resp1.innerText = numPendentes
 }
+window.addEventListener("load", mostrarPendentes)
 
 frm.btExecutar.addEventListener("click", () => {
-    const vetorPrimario = localStorage.getItem("serviço").split(";")
-    const servicoEmExecucao = vetorPrimario.shift()
-    localStorage.setItem("serviço", vetorPrimario.join(";"))
-    removerServicos(servicoEmExecucao)
-    
+
+  if (!localStorage.getItem("herbieServico")) { 
+    alert("Não há serviços pendentes para executar")
+    return
+  }
+  const servicos = localStorage.getItem("herbieServico").split(";")
+  const emExecucao = servicos.shift()
+  resp2.innerText = emExecucao 
+  localStorage.setItem("herbieServico", servicos.join(";"))
+  mostrarPendentes()
 })
-
-const removerServicos = (servicoEmExecucao) => {
-    const servicos = localStorage.getItem("serviço").split(";")
-    console.log(localStorage.getItem("serviço"))
-    numServicos.innerText = servicos == [''] ? 0 : servicos.length 
-    tipoServico.innerHTML = servicoEmExecucao == "" ? "Não há serviços" : servicoEmExecucao
-}
-
-window.addEventListener("load", adicionarServicos)
-window.addEventListener("load", removerServicos)
-
-
