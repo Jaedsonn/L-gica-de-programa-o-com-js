3a83d6fd7499a0f87b2fb5b9c988e89eb5fb9a17
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-02-27 19:51:47 -0300

    adicionando a nova funcionalidade ao ex 10_1

--- livrojs/cap10/js/ex10_1.js
+++ livrojs/cap10/js/ex10_1.js
@@ -14,3 +14,32 @@ frm.addEventListener("submit", (e) =>{
     frm.inTarefa.value = ""
     frm.inTarefa.focus()
 })
+
+frm.btSelecionar.addEventListener("click", ()=>{
+    const tarefas = document.querySelectorAll("h5")
+
+    if(tarefas.length == 0){
+        alert("Não há tarefas para selecionar")
+        return
+    }
+
+    let aux = -1 // varíavel auxiliar para indicar a linha selecionada
+
+    //percorre a lista de elementos h5 inseridos na página, ou seja, tarefas
+    for(let i = 0; i < tarefas.length;i++){
+        //se taf é da class tarefa-selecionada (está selecionada)
+        if(tarefas[i].className == "tarefa-selecionada"){
+            tarefas[i].className = "tarefa-normal"
+            aux = i
+            break
+        }
+    }
+
+    // se a linha que está selecionada é a última, irá voltar para a primeira linha
+
+    if(aux == tarefas.length - 1){
+        aux = -1
+    }
+
+    tarefas[aux + 1].className = "tarefa-selecionada"
+})
