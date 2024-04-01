67ca685f2c7d250bc34c4c827fe9033bb84e4a3a
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-04-01 14:27:32 -0300

    vai na fé

--- livrojs/cap13/src/App.js
+++ livrojs/cap13/src/App.js
@@ -1,14 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import MenuSuperior from "./components/MenuSuperior";
-import InclusãoLivros from "./components/InclusaoLivros";
+import InclusaoLivros from "./components/InclusaoLivros";
+import ManutencaoLivros from "./components/ManutencaoLivros";
+import ResumoLivros from "./components/ResumoLivros";
 
-
-function App() {
+const App = () => {
   return (
-  <>
-  <MenuSuperior/>
-  <InclusãoLivros/>
-  </>
+    <>
+      <MenuSuperior />
+      <Routes>
+        <Route path="/" element={<InclusaoLivros />} />
+        <Route path="manut" element={<ManutencaoLivros />} />
+        <Route path="resumo" element={<ResumoLivros />} />
+      </Routes>
+    </>
   );
-}
-
+};
 export default App;
